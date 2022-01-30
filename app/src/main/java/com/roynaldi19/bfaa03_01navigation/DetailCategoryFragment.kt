c336ed886944d838df779bb8150e80db50264eec
roynaldi19 <roynaldi19@gmail.com>
2022-01-30 13:59:24 +0700

    Initial commit dam kirim data dengan bundle

---
+++ app/src/main/java/com/roynaldi19/bfaa03_01navigation/DetailCategoryFragment.kt
@@ -0,0 +1,40 @@
+package com.roynaldi19.bfaa03_01navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.roynaldi19.bfaa03_01navigation.databinding.FragmentDetailCategoryBinding
+
+
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
