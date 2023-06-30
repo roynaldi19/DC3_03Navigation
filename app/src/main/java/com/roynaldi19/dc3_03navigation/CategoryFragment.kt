0d70004e9ab43af1944298fbc46bd242ecbe8566
roynaldi19 <roynaldi19@gmail.com>
2023-06-30 21:11:06 +0700

    navigasi ke categoryFrgament dan profileActivity

---
+++ app/src/main/java/com/roynaldi19/dc3_03navigation/CategoryFragment.kt
@@ -0,0 +1,19 @@
+package com.roynaldi19.dc3_03navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CategoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+}
